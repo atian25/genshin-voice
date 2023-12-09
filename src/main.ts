5d142e0d2d6d9fc75185f1ae78a681a655316b51
TZ <atian25@qq.com>
2023-12-09 10:01:43 +0800

    feat: first init

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
 // For more information, see https://crawlee.dev/
-import { CheerioCrawler, ProxyConfiguration } from 'crawlee';
+import { CheerioCrawler, ProxyConfiguration, Dataset } from 'crawlee';
 
 import { router } from './routes.js';
 
-const startUrls = ['https://crawlee.dev'];
+const startUrls = ['https://wiki.biligame.com/ys/%E8%A7%92%E8%89%B2%E8%AF%AD%E9%9F%B3'];
 
 const crawler = new CheerioCrawler({
-    // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
-    requestHandler: router,
-    // Comment this option to scrape the full website.
-    maxRequestsPerCrawl: 20,
+  // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
+  requestHandler: router,
+  // Comment this option to scrape the full website.
+  // maxRequestsPerCrawl: 20,
 });
 
 await crawler.run(startUrls);
+
+const dataset = await Dataset.open();
+await dataset.exportToJSON('MY-DATA');
--- src/routes.ts
+++ src/routes.ts
@@ -1,21 +1,54 @@
-import { createCheerioRouter } from 'crawlee';
+import { createCheerioRouter, KeyValueStore } from 'crawlee';
 
 export const router = createCheerioRouter();
 
-router.addDefaultHandler(async ({ enqueueLinks, log }) => {
-    log.info(`enqueueing new URLs`);
-    await enqueueLinks({
-        globs: ['https://crawlee.dev/**'],
-        label: 'detail',
-    });
+const store = await KeyValueStore.open('genshin');
+
+router.addDefaultHandler( async ctx => {
+  const { request, $, log, pushData } = ctx;
+  const title = $('title').text();
+  log.info(`${title}`, { url: request.loadedUrl });
+
+  const list = $('.resp-tabs-container .home-box-tag .iteminfo .home-box-tag-1 a[title]');
+  const urls = new Set<string>();
+  for (const item of list) {
+    const { title, href } = item.attribs;
+    if (!title.startsWith('旅行者')) {
+      urls.add(href);
+    }
+  }
+
+  log.info(`Total: ${urls.size}`);
+
+  ctx.enqueueLinks({
+    urls: [...urls],
+    label: 'detail',
+  });
 });
 
 router.addHandler('detail', async ({ request, $, log, pushData }) => {
-    const title = $('title').text();
-    log.info(`${title}`, { url: request.loadedUrl });
+  const title = $('title').text();
+  log.info(`${title}`, { url: request.loadedUrl });
 
-    await pushData({
-        url: request.loadedUrl,
-        title,
-    });
+  const name = title.substring(0, title.indexOf('语音'));
+
+  const result = {
+    name,
+    voices: [],
+  };
+
+  const list = $('.resp-tabs-container .wikitable');
+  for (const item of list) {
+    const data = {
+      topic: $(item).find('th').first().text().trim(),
+      chs: $(item).find('.voice_text_chs_m').text().trim(),
+      jp: $(item).find('.voice_text_jp_m').text().trim(),
+      en: $(item).find('.voice_text_en_m').text().trim(),
+      kr: $(item).find('.voice_text_kr_m').text().trim(),
+    };
+    result.voices.push(data);
+    // log.info('%j', data);
+  }
+
+  pushData(result);
 });
